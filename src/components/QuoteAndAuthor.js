afce68f3118998c9397dbf95b6a113fbe9b44f5d
Zahra <hizahrazadeh@gmail.com>
2021-03-01 16:38:38 -0500

    twitter button

--- src/components/QuoteAndAuthor.js
+++ src/components/QuoteAndAuthor.js
@@ -9,10 +9,19 @@ export default function QuoteAndAuthor(props) {
             <div className="box-body">
                 <p className="quote-text">{quote.quote}</p>
                 <h6 className="author-text">{quote.author}</h6>
+                <div className="buttons"> 
                 <button
                     onClick={() => {generateRandomQuote(quotes) }}
                     type="submit">
                     <i class="fas fa-mouse"></i>New quote</button>
+                <button
+                    className="ml-3"
+                    onClick={() => { generateRandomQuote(quotes);
+                    window.open('https://twitter.com/intent/tweet/?text='
+                    + encodeURIComponent(quote.quote + '--' + quote.author))
+                    }}
+                    type="submit"><i class="fab fa-twitter"></i>Share Quote</button>
+                </div>
             </div>
         </div>
     )
