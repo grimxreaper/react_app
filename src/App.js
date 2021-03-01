ba77f44942a9ef672176a5062e7e9fa2f844398f
Zahra <hizahrazadeh@gmail.com>
2021-03-01 16:24:32 -0500

    migrated file and started over

--- src/App.js
+++ src/App.js
@@ -1,3 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import QuoteAndAuthor from './components/QuoteAndAuthor';
 import quotes from './QuoteDB';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: quotes[0].quote,
+      author: quotes[0].author
+  }
+
+  addQuote = (arr) => {
+    let index = Math.floor(Math.random() * (quotes.length));
+    let newQuote = quotes[index]
+    //update state
+    this.setState({
+      quote: newQuote.quote, 
+      author: newQuote.author
+    })
+
+    this.shuffleQuotes(quotes)
+  }
+
+  // shuffleQuotes = (arr) => {
+  //   return arr.sort(function () { return 0.5 - Math.random() });
+  // }
+
+  render () {
+    return (
+      <div className="container">
+        <QuoteAndAuthor generateRandomQuote = {this.generateRandomQuote}
+        quote={this.state}
+        />
+      </div>
+    )
+  }
+
+
+
+
+
+
+
+}
